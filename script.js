39bf588e9ea4833dadbb38fe21a271259f210e78
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2024-12-29 21:08:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // write your code 
+	s1=s1.toLowerCase();
+	s2=s2.toLowerCase();
+	let result=s1.indexOf(s2);
+	return result;
 }
 
 // Please do not change the code below
